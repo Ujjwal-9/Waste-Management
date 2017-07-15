ee1b7f567367d0c429033c35200a7e6f8d9a6f4a
Ujjwal <ujjwalupadhyay8@gmail.com>
2017-07-16 04:11:39 +0530

    ui yet to be made

---
+++ clarifai-android-starter/app/src/main/java/com/clarifai/android/starter/api/v2/activity/RecognizeConceptsActivity.java
@@ -0,0 +1,171 @@
+package com.clarifai.android.starter.api.v2.activity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+import butterknife.BindView;
+import butterknife.OnClick;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import com.clarifai.android.starter.api.v2.App;
+import com.clarifai.android.starter.api.v2.ClarifaiUtil;
+import com.clarifai.android.starter.api.v2.R;
+import com.clarifai.android.starter.api.v2.adapter.RecognizeConceptsAdapter;
+
+import java.util.Collections;
+import java.util.List;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public final class RecognizeConceptsActivity extends BaseActivity {
+
+  public static final int PICK_IMAGE = 100;
+
+  // the list of results that were returned from the API
+  @BindView(R.id.resultsList) RecyclerView resultsList;
+
+  // the view where the image the user selected is displayed
+  @BindView(R.id.image) ImageView imageView;
+
+  // switches between the text prompting the user to hit the FAB, and the loading spinner
+  @BindView(R.id.switcher) ViewSwitcher switcher;
+  // the FAB that the user clicks to select an image
+  @BindView(R.id.fab) View fab;
+
+  @BindView(R.id.steps) TextView steps;
+
+  @NonNull private final RecognizeConceptsAdapter adapter = new RecognizeConceptsAdapter();
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+
+    resultsList.setLayoutManager(new LinearLayoutManager(this));
+    resultsList.setAdapter(adapter);
+  }
+
+  @OnClick(R.id.fab)
+  void pickImage() {
+    startActivityForResult(new Intent(Intent.ACTION_PICK).setType("image/*"), PICK_IMAGE);
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode != RESULT_OK) {
+      return;
+    }
+    switch(requestCode) {
+      case PICK_IMAGE:
+        final byte[] imageBytes = ClarifaiUtil.retrieveSelectedImage(this, data);
+        if (imageBytes != null) {
+          onImagePicked(imageBytes);
+        }
+        break;
+    }
+  }
+
+  private void onImagePicked(@NonNull final byte[] imageBytes) {
+    // Now we will upload our image to the Clarifai API
+    setBusy(true);
+
+    // Make sure we don't show a list of old concepts while the image is being uploaded
+    adapter.setData(Collections.<Concept>emptyList());
+
+    new AsyncTask<Void, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
+      @Override protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Void... params) {
+        // The default Clarifai model that identifies concepts in images
+        final ConceptModel generalModel = App.get().clarifaiClient().getDefaultModels().generalModel();
+
+        // Use this model to predict, with the image that the user just selected as the input
+        return generalModel.predict()
+            .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(imageBytes)))
+            .executeSync();
+      }
+
+      @Override protected void onPostExecute(ClarifaiResponse<List<ClarifaiOutput<Concept>>> response) {
+        setBusy(false);
+        if (!response.isSuccessful()) {
+          showErrorSnackbar(R.string.error_while_contacting_api);
+          return;
+        }
+        final List<ClarifaiOutput<Concept>> predictions = response.get();
+        if (predictions.isEmpty()) {
+          showErrorSnackbar(R.string.no_results_from_api);
+          return;
+        }
+        adapter.setData(predictions.get(0).data());
+        imageView.setImageBitmap(BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length));
+        steps.setText("");
+        int cnt=1;
+        // water rodents recyclable non recyclable
+        boolean[] arr={false,false,false,false};
+        String[] values={"Use disinfectant on water.\nClean water from there using gloves. (Refer Here)","Use rodent control techniques. (Refer Here)",
+                "Filter recyclable materials. (Refer Here)","Handle other kind of waste. (Refer Here)"};
+        for(int i=0;i<predictions.get(0).data().size();i++){
+          Concept concept=predictions.get(0).data().get(i);
+          //steps.setText(steps.getText().toString()+concept.name()+"\n");
+          String str=concept.name();
+          if(str.equals("water") || str.equals("wet")){
+            arr[0]=true;
+          }
+          if(str.equals("insect") || str.equals("invertebrate")){
+            arr[1]=true;
+          }
+          if(str.equals("recycling") || str.equals("junk") || str.equals("junkyard")){
+            arr[2]=true;
+          }
+          if(str.equals("trash") || str.equals("garbage") || str.equals("waste")){
+            arr[3]=true;
+          }
+        }
+        for(int i=0;i<4;i++){
+          if(arr[i]){
+            steps.setText(steps.getText().toString()+"Step "+cnt+": "+values[i]+"\n");
+            cnt++;
+          }
+        }
+      }
+
+      private void showErrorSnackbar(@StringRes int errorString) {
+        Snackbar.make(
+            root,
+            errorString,
+            Snackbar.LENGTH_INDEFINITE
+        ).show();
+      }
+    }.execute();
+  }
+
+  @Override protected int layoutRes() { return R.layout.activity_recognize; }
+
+  private void setBusy(final boolean busy) {
+    runOnUiThread(new Runnable() {
+      @Override public void run() {
+        switcher.setDisplayedChild(busy ? 1 : 0);
+        steps.setVisibility(busy?GONE:VISIBLE);
+        imageView.setVisibility(busy ? GONE : VISIBLE);
+        fab.setEnabled(!busy);
+      }
+    });
+  }
+
+}
